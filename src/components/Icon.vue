b10258971d6b92e498d3bbf2a0d85ce7025248b4
Jan den Besten <info@jandenbesten.net>
2021-02-16 09:44:31 +0100

    Eigen icon component voor ion-iocon

---
+++ src/components/Icon.vue
@@ -0,0 +1,26 @@
+<template>
+  <ion-icon :src="src" :size="size"></ion-icon>
+</template>
+
+<script>
+  export default {
+    name: 'Icon',
+
+    props: {
+      'name':{
+        type: String,
+        default:'',
+      },
+      'size':{
+        type:String,
+        default:'',
+      },
+    },
+
+    computed : {
+      src() {
+        return 'assets/svg/'+this.name+'.svg';
+      },
+    },
+};
+</script>
